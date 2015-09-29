4114561545dc8b594c75ad2452f6dd9a7e49ba97
Manuel Leduc <manuel.leduc@inria.fr>
2015-09-29 10:43:22 +0200

    Cleanup
    Format and useless import removed

--- NugetLoader.cs
+++ NugetLoader.cs
@@ -1,10 +1,6 @@
 ﻿using Org.Kevoree.Core.Api;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Org.Kevoree.NugetLoader
 {
